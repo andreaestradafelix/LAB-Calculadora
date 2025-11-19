87cc64bf7abf80c45ef9b01127e1818df897437a
Andrea Estrada <andreaestradafelix@gmail.com>
2025-11-19 12:47:15 -0700

    Ejercicio Calculadora

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let a = parseFloat(prompt("Ingrese el primer número:"));
+let b = parseFloat(prompt("Ingrese el segundo número:"));
+
+function sumar(a, b) {
+    return a + b;
+}
+
+function restar(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+function dividir(a, b) {
+   return a/b;
+}
+
+console.log("Suma: " + sumar(a, b));
+console.log("Resta: " + restar(a, b));
+console.log("Multiplicación: " + multiplicar(a, b));
+console.log("División: " + dividir(a, b));
